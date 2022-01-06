952f971f6dcc6b8832b5d75cb110b7c27f3d4e10
Samuli Jarvinen <frye@github.com>
2022-01-05 20:53:25 -0800

    Initial working version

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,10 @@ function generatePassword() {
   const MIN_PWLENGTH = 8;
   const MAX_PWLENGTH = 124; //might remove this later. I guess we could just go on ang generate even longer passwords.
   var characterString = ''; // We will add the character classes here based on user feedback and then use it to generate the PW.
-
+  var passwordString = '';
   var passwordLength = MIN_PWLENGTH; //initial length, can be overridden by user.
+
+  // Prompts to get user input on password length and what character classes to include.
   passwordLength = prompt('Enter desired password length: ', MIN_PWLENGTH);
   if (passwordLength < MIN_PWLENGTH || passwordLength > MAX_PWLENGTH) {
     alert('Entered password lengt is not between 8 and 124 characters. Using default: ' + MIN_PWLENGTH);
@@ -48,6 +50,16 @@ function generatePassword() {
 
   console.log(characterString);
 
+  // Generate the actual password.
+
+  for (var i = 0; i < passwordLength; i++ ) {
+    var index = Math.floor(Math.random() * characterString.length);
+    passwordString += characterString.substring(index, index + 1);
+    console.log(passwordString);
+  }
+  console.log(passwordString)
+  // Return the generated password to be added into the webpage.
+  return passwordString;
 }
 
 // Write password to the #password input
